a46261d910af9a5080486eb91b361f1dc4623462
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-29 22:36:38 +0200

    Travis CI

--- build/createimage.sh
+++ build/createimage.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 IMAGE=image.bin
 dd if=/dev/zero of=$IMAGE bs=1k count=16384
-mke2fs -t ext3 -f $IMAGE
-mount -o loop $IMAGE /mnt
-cp -r /source /mnt
-umount /mnt
+sudo mke2fs -t ext3 $IMAGE
+sudo mount -o loop $IMAGE /mnt
+cp -r ./ /mnt
+sudo umount /mnt
 

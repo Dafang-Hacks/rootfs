6abe4ff735055fb978514830a8a3600146b4d611
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-29 23:23:42 +0200

    Create Image

--- build/createimage.sh
+++ build/createimage.sh
@@ -8,7 +8,11 @@ echo "DIRSIZE: $DIRSIZE"
 IMAGE=/tmp/image.bin
 dd if=/dev/zero of=$IMAGE bs=1k count=$DIRSIZE
 sudo mke2fs -t ext3 -F $IMAGE
-sudo mount -o loop $IMAGE /mnt
-sudo rsync -r -a --exclude '.git' ../* /mnt
-sudo umount /mnt
+mkdir mnt
+sudo mount -o loop $IMAGE mnt
+sudo rsync -r -a --exclude '.git' ../* mnt
+sudo umount mnt
 
+## Delete everything:
+sudo rm -r ../*
+mv $IMAGE ../image.bin
